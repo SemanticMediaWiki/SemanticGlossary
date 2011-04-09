67680e8ebe6417acdc85846b6dbd20ccdd823b2d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-04-09 18:03:14 +0000

    Update L10n for SemanticGlossary. Add support for SemanticGlossary to Translate.

--- SemanticGlossary.i18n.php
+++ SemanticGlossary.i18n.php
@@ -20,15 +20,15 @@ $messages['en'] = array(
 	'semanticglossary-enternewterm' => 'You can enter a new term and definition here:',
 
 	'semanticglossary-messageheader' => 'Messages:',
-	'semanticglossary-storedtermdefinedinarticle' => 'The term \'$1\' was originally defined in article [[$2]]. The definition was changed as required for now. However, as soon as the original article is edited again, the definition there takes precedence.',
-	'semanticglossary-deletedtermdefinedinarticle' => 'The term \'$1\' was originally defined in article [[$2]]. The definition was deleted as required for now. However, as soon as the original article is edited again, the definition there takes precedence.',
+	'semanticglossary-storedtermdefinedinarticle' => 'The term "$1" was originally defined in page [[$2]]. The definition was changed as required for now. However, as soon as the original page is edited again, the definition there takes precedence.',
+	'semanticglossary-deletedtermdefinedinarticle' => 'The term "$1" was originally defined in page [[$2]]. The definition was deleted as required for now. However, as soon as the original page is edited again, the definition there takes precedence.',
 	'semanticglossary-termdeleted' => 'Deleted $1.',
-	'semanticglossary-storedtermdefinedtwice' => 'The article [[$1]] contains more than one property named $2. Will not store data for term \'$3\'.',
-	'semanticglossary-termdefinedtwice' => 'The article [[$1]] contains more than one term and/or more than one definition. The entries will not be available for the glossary.',
+	'semanticglossary-storedtermdefinedtwice' => 'The page [[$1]] contains more than one property named $2. Will not store data for term "$3".',
+	'semanticglossary-termdefinedtwice' => 'The page [[$1]] contains more than one term and/or more than one definition. The entries will not be available for the glossary.',
 
 	'semanticglossary-brokensession' => 'Action not allowed. Broken session data.',
 	'semanticglossary-norights' => 'Action not allowed. Insufficient rights.',
-	
+
 	'___glt' => 'Glossary-Term',
 	'___gld' => 'Glossary-Definition',
 
@@ -50,7 +50,7 @@ $messages['de'] = array(
 	'semanticglossary-deleteselected' => 'Ausgewählte löschen',
 	'semanticglossary-savechanges' => 'Änderungen speichern',
 	'semanticglossary-createnew' => 'Neuen Term anlegen',
-	
+
 	'semanticglossary-termsdefined' => 'Diese Begriffe sind im Wiki definiert:',
 	'semanticglossary-notermsdefined' => 'Es sind derzeit keine Begriffe im Wiki definiert.',
 	'semanticglossary-enternewterm' => 'Du kannst hier einen neuen Term mit Definition eingeben:',
