1a5213066de792acfb2c79b35584482e114f1ff8
Hermann Schwärzler <hermann.schwaerzler@uibk.ac.at>
2019-01-10 09:14:36 +0100

    Load Extension:Lingo only if not already loaded (#43) (#44)

--- SemanticGlossary.php
+++ SemanticGlossary.php
@@ -19,12 +19,15 @@ class SemanticGlossary {
 			require_once __DIR__ . '/vendor/autoload.php';
 		}
 
-		// must NOT use ExtensionRegistry::getInstance() to avoid recursion!
-		$registry = new ExtensionRegistry();
-		if ( file_exists( __DIR__ . '/extensions/Lingo/extension.json' ) ) {
-			$registry->load( __DIR__ . '/extensions/Lingo/extension.json' );
-		} else {
-			$registry->load( $GLOBALS[ 'wgExtensionDirectory' ] . '/Lingo/extension.json' );
+		// Load Extension:Lingo - but only if not already loaded
+		if ( ! class_exists( 'Lingo\Lingo' ) ) {
+			// must NOT use ExtensionRegistry::getInstance() to avoid recursion!
+			$registry = new ExtensionRegistry();
+			if ( file_exists( __DIR__ . '/extensions/Lingo/extension.json' ) ) {
+				$registry->load( __DIR__ . '/extensions/Lingo/extension.json' );
+			} else {
+				$registry->load( $GLOBALS[ 'wgExtensionDirectory' ] . '/Lingo/extension.json' );
+			}
 		}
 
 		$GLOBALS[ 'wgexLingoBackend' ] = 'SG\LingoBackendAdapter';
