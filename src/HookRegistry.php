9ec1e6880c7fc7ea6c61a39852fb69d843370686
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-09-27 15:14:33 +0200

    fix sniff MediaWiki.Usage.StaticClosure.StaticClosure

--- src/HookRegistry.php
+++ src/HookRegistry.php
@@ -61,7 +61,7 @@ class HookRegistry {
 		/**
 		 * @see https://github.com/SemanticMediaWiki/SemanticMediaWiki/blob/master/docs/technical/hooks.md
 		 */
-		$this->handlers['SMW::Property::initProperties'] = function ( $propertyRegistry ) {
+		$this->handlers['SMW::Property::initProperties'] = static function ( $propertyRegistry ) {
 			$propertyRegistrationHelper = new PropertyRegistrationHelper( $propertyRegistry );
 			return $propertyRegistrationHelper->registerProperties();
 
@@ -72,7 +72,7 @@ class HookRegistry {
 		 *
 		 * @since 1.0
 		 */
-		$this->handlers['SMWStore::updateDataBefore'] = function ( $store, $semanticData ) {
+		$this->handlers['SMWStore::updateDataBefore'] = static function ( $store, $semanticData ) {
 			return \SG\Cache\CacheInvalidator::getInstance()->invalidateCacheOnStoreUpdate( $store, $semanticData );
 		};
 
@@ -81,7 +81,7 @@ class HookRegistry {
 		 *
 		 * @since 1.0
 		 */
-		$this->handlers['SMW::SQLStore::AfterDeleteSubjectComplete'] = function ( $store, $title ) {
+		$this->handlers['SMW::SQLStore::AfterDeleteSubjectComplete'] = static function ( $store, $title ) {
 			return \SG\Cache\CacheInvalidator::getInstance()->invalidateCacheOnPageDelete(
 				$store,
 				DIWikiPage::newFromTitle( $title )
@@ -94,11 +94,11 @@ class HookRegistry {
 		 * @since 1.0
 		 */
 		if ( version_compare( MW_VERSION, "1.35.0", "<" ) ) {
-			$this->handlers['TitleMoveComplete'] = function ( &$old_title ) {
+			$this->handlers['TitleMoveComplete'] = static function ( &$old_title ) {
 				return \SG\Cache\CacheInvalidator::getInstance()->invalidateCacheOnPageMove( $old_title );
 			};
 		} else {
-			$this->handlers['PageMoveComplete'] = function ( LinkTarget $old_title ) {
+			$this->handlers['PageMoveComplete'] = static function ( LinkTarget $old_title ) {
 				return \SG\Cache\CacheInvalidator::getInstance()->invalidateCacheOnPageMove( $old_title );
 			};
 		}
